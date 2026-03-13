43dfa350f9e9e60fa5cbc69d94148a96e30baa74
Aravind Nelavelli <nelavelliaravind@gmail.com>
2026-03-13 08:36:57 +0530

    Delete SkillInsem directory

--- SkillInsem/src/main/java/com/klef/fsad/exam/repository/SupplierOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klef.fsad.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klef.fsad.exam.model.SupplierOrder;
-
-public interface SupplierOrderRepository extends JpaRepository<SupplierOrder, Integer>
-{
-
-}
